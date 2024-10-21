88452834a03a343d39aff9bc4a9915d170a16ae5
ZydrunasK <kniukstaz@gmail.com>
2024-10-21 18:01:59 +0300

    up

--- server/api/registerAPI.js
+++ server/api/registerAPI.js
@@ -1,42 +1,18 @@
 import { IsValid } from '../lib/IsValid.js';
-import {connection} from '../db.js';
 
-export async function registerPostAPI(req, res) {
+export function registerPostAPI(req, res) {
     const requiredFields = [
-        {
-            field: 'email',
-            validation: IsValid.email,
-        },
-        {
-            field: 'password',
-            validation: IsValid.password,
-        },
+        { field: 'email', validation: IsValid.email },
+        { field: 'password', validation: IsValid.password },
     ];
 
-    if (Object.keys(req.body).length !== requiredFields.length) {
+    const [isErr, errMessage] = IsValid.requiredFields(req.body, requiredFields);
+    if (isErr) {
         return res.json({
             status: 'error',
-            msg: 'Registracijai reikalingi tik email ir password',
+            msg: errMessage,
         });
     }
-
-    for (const { field, validation } of requiredFields) {
-        const value = req.body[field];
-        const [isErr, errMessage] = validation(value);
-
-        if (isErr) {
-            return res.json({
-                status: 'error',
-                msg: errMessage,
-            });
-        }
-    }
-
-    const sql = 'SELECT * FROM users;';
-    const selectResult = await connection.execute(sql);
-
-    console.log(selectResult[0]);
-    
 
     return res.json({
         status: 'success',
