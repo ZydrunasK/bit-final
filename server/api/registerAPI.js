cfb8fe1c07fdd79f53540fb36f308867808b0ced
ZydrunasK <kniukstaz@gmail.com>
2024-10-22 08:25:00 +0300

    up

--- server/api/registerAPI.js
+++ server/api/registerAPI.js
@@ -1,6 +1,7 @@
+import { connection } from '../db.js';
 import { IsValid } from '../lib/IsValid.js';
 
-export function registerPostAPI(req, res) {
+export async function registerPostAPI(req, res) {
     const requiredFields = [
         { field: 'email', validation: IsValid.email },
         { field: 'password', validation: IsValid.password },
@@ -12,6 +13,27 @@ export function registerPostAPI(req, res) {
             status: 'error',
             msg: errMessage,
         });
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        const sql = `SELECT * FROM users WHERE email = ?`;
+        const selectResult = await connection.execute(sql, [email]);
+        console.log(selectResult[0]);
+        
+        if (selectResult[0].lenght > 0) {    
+            return res.json({
+                status: 'error',
+                msg: 'Toks Email jau yra panaudotas',
+            });
+        }
+        
+    } catch (error) {
+        return res.json({
+            status: 'error',
+            msg: 'Server error',
+        });       
     }
 
     return res.json({
--- server/db.js
+++ server/db.js
@@ -1,9 +1,17 @@
-import mysql2 from 'mysql2/promise';
+import mysql2 from "mysql2/promise";
 
+export let connection = null;
 
-export const connection = await mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: '51gr_bit-final',
-});
+try {
+    console.log('hi');
+    
+    connection = await mysql2.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: '51gr_bit-final',
+    });
+} catch (error) {
+    console.log(error);
+}
+
